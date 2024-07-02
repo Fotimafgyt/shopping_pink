c00fdcfb67468a91dea177f535f2eb9bf916386c
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-07-02 19:10:32 +0500

    123
    122

---
+++ data.js
@@ -0,0 +1,162 @@
+data=[
+    {
+      "id": 1,
+      "title": "Hydrating Face Mask",
+      "category": "Skincare",
+      "price": 29.99,
+      "image": "https://www.lakmeindia.com/cdn/shop/files/27668_H-8901030943577.jpg"
+    },
+    {
+      "id": 2,
+      "title": "Volumizing Shampoo",
+      "category": "Haircare",
+      "price": 17.99,
+      "image": "https://target.scene7.com/is/image/Target/GUEST_0d1cb61f-e617-46de-b816-6050252327a8?qlt=65&fmt=pjpeg&hei=350&wid=350"
+    },
+    {
+      "id": 3,
+      "title": "Brightening Eye Cream",
+      "category": "Skincare",
+      "price": 35.99,
+      "image": "https://caudalie-americas.imgix.net/media/catalog/product/1/_/1_vinoperfect_eye_cream_caudalie_packshot_offwhite.jpg?auto=format,compress&cs=srgb&fm=auto&w=1200"
+    },
+    {
+      "id": 4,
+      "title": "Clarifying Facial Cleanser",
+      "category": "Skincare",
+      "price": 22.99,
+      "image": "https://cdn-mms.hktvmall.com/hktv/mms/uploadProductImage/c6cd/146d/ec42/vpfSiHpElI20230417105255_515.jpg"
+    },
+    {
+      "id": 5,
+      "title": "Smoothing Body Scrub",
+      "category": "Body Care",
+      "price": 18.99,
+      "image": "https://eu.medik8.com/cdn/shop/products/SmoothBodyExfoliatingKit-WithMit-WithCarton-GreyBackground-2000x2000-ROW.jpg?crop=center&height=2048&v=1614947408&width=2048"
+    },
+    {
+      "id": 6,
+      "title": "Repairing Hair Mask",
+      "category": "Haircare",
+      "price": 24.99,
+      "image": "https://manemethod.co/cdn/shop/products/DSC_0121-min-sq_900x.png?v=1623692731"
+    },
+    {
+      "id": 7,
+      "title": "Soothing Facial Mist",
+      "category": "Skincare",
+      "price": 15.99,
+      "image": "https://www.gosupps.com/media/catalog/product/cache/25/image/1500x/040ec09b1e35df139433887a97daa66f/9/1/91YkweNjvCL._SL1500_.jpg"
+    },
+    {
+      "id": 8,
+      "title": "Nourishing Body Butter",
+      "category": "Body Care",
+      "price": 29.99,
+      "image": "https://m.media-amazon.com/images/I/91Ks3AkFUjL.jpg"
+    },
+    {
+      "id": 9,
+      "title": "Volumizing Hair Spray",
+      "category": "Haircare",
+      "price": 12.99,
+      "image": "https://rizoscurls.com/cdn/shop/files/RizosCurls_HairSpray_RedAlgae_RepairHair.webp?v=1700285314"
+    },
+    {
+      "id": 10,
+      "title": "Purifying Clay Mask",
+      "category": "Skincare",
+      "price": 27.99,
+      "image": "https://sothys.uz/thumb/2/2j-MNfK6Hri5l-cjPLQdsA/r/d/masque_purifiant_aux_deux_argiles_sothys_paris_2_3532.jpg"
+    },
+    {
+      "id": 11,
+      "title": "Nourishing Hand Cream",
+      "category": "Body Care",
+      "price": 14.99,
+      "image": "https://bio2you.lv/wp-content/uploads/2022/09/B2Y_summer19_fullsize-49-1.jpg"
+    },
+    {
+      "id": 12,
+      "title": "Strengthening Nail Polish",
+      "category": "Nails",
+      "price": 10.99,
+      "image": "https://beter.es/6188-thickbox_default/strengthening-nail-polish-orchid.jpg"
+    },
+    {
+      "id": 13,
+      "title": "Renewing Lip Scrub",
+      "category": "Lip Care",
+      "price": 8.99,
+      "image": "https://images.richreachapps.com/caprichreach/public/ACC_26/account/product/7081975/7081975_A4.jpeg"
+    },
+    {
+      "id": 14,
+      "title": "Hydrating Body Oil",
+      "category": "Body Care",
+      "price": 24.99,
+      "image": "https://skyorganics.com/cdn/shop/files/2NC-XBODYOIL6ListingAssetAMAZONHero_1200x.jpg?v=1690214431"
+    },
+    {
+      "id": 15,
+      "title": "Smoothing Hair Serum",
+      "category": "Haircare",
+      "price": 19.99,
+      "image": "https://static.ksisters.com/public/skus/l/eacdd95f1351bdb83ff1a6f7f549d75f_039kaam6_e7dac9a6cedcdf.webp"
+    },
+    {
+      "id": 16,
+      "title": "Brightening Vitamin C Serum",
+      "category": "Skincare",
+      "price": 39.99,
+      "image": "https://tribeskincare.com.au/cdn/shop/products/1_f8ccb6a7-4386-465c-b4b3-adfe3fc9d60c.jpg?v=1681440310"
+    },
+    {
+      "id": 17,
+      "title": "Nourishing Cuticle Oil",
+      "category": "Nails",
+      "price": 12.99,
+      "image": "https://cdn.shopify.com/s/files/1/2239/6447/t/199/assets/NourishingCuticleOil-lifestyle2s1-1666772342536.jpg"
+    },
+    {
+      "id": 18,
+      "title": "Exfoliating Foot Scrub",
+      "category": "Body Care",
+      "price": 16.99,
+      "image": "https://i.pinimg.com/736x/50/93/28/509328fae8e452391314c37bcba9be1e.jpg"
+    },
+    {
+      "id": 19,
+      "title": "Volumizing Dry Shampoo",
+      "category": "Haircare",
+      "price": 14.99,
+      "image": "https://myhairdance.com/cdn/shop/products/DSC00107_3.jpg?v=1665441620"
+    },
+    {
+      "id": 20,
+      "title": "Anti-Aging Serum",
+      "category": "Skincare",
+      "price": 45.99,
+      "image": "https://rukminim2.flixcart.com/image/850/1000/xif0q/anti-ageing/a/m/i/30-anti-ageing-serum-enriched-for-glowing-spotless-anti-ageing-original-imaghrstqdmfmyns.jpeg?q=90&crop=false"
+    }
+  ]
+  data.map((item,key)=>{
+    document.querySelector('.cards_product').innerHTML+=`
+    <div class="card_product">
+    <div class="card_product_img">
+        <img src="${item.image}" alt="">
+    </div>
+    <div class="card_product_body">
+        <div class="catd_product_title">${item.title}</div>
+        <div class="catd_product_price">${item.price}</div>
+        <div class="catd_product_button">
+            <button>buy</button>
+        </div>
+    </div>
+        </div>
+    `
+})
+
+function  search1() {
+  
+}
